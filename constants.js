41191f68679b8c97e407936adbb40d7c5e3c4fce
Ferxu01 <ferortjim@gmail.com>
2022-07-23 12:11:23 +0200

    Setting route
    Preparing API routes

---
+++ constants.js
@@ -0,0 +1,7 @@
+const routeURL = __dirname + '\\routes\\';
+
+const projectsURL = routeURL + 'projects';
+
+module.exports = {
+    projectsURL: projectsURL
+};
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const constants = require('./constants');
 
 const app = express();
 
 //Configuration variables
 app.set('port', 3000);
 app.set(express.json());
+
+//Routes
+const projectsRoute = require(constants.projectsURL);
+
+//Routers
+app.use('/projects', projectsRoute);
+
 
 app.listen(app.get('port'));
